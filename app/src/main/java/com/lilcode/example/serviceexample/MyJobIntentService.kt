04696a67a3264278fc36a1bf8cafd954d1a22b96
NetLSS <futureprogrammer@naver.com>
2021-08-26 22:43:59 +0900

    Feat: Service 클래스 생성

---
+++ app/src/main/java/com/lilcode/example/serviceexample/MyJobIntentService.kt
@@ -0,0 +1,12 @@
+package com.lilcode.example.serviceexample
+
+import android.content.Intent
+import androidx.core.app.JobIntentService
+
+// Deprecated 되었음.. WorkManager 에 대해서 알아볼것.
+class MyJobIntentService : JobIntentService(){
+    override fun onHandleWork(intent: Intent) {
+
+    }
+
+}
