eaa964f820db0aea70aeac1c74cf324f4536987a
NetLSS <futureprogrammer@naver.com>
2021-08-26 22:46:31 +0900

    Feat: onHandleWork override 하기

--- app/src/main/java/com/lilcode/example/serviceexample/MyJobIntentService.kt
+++ app/src/main/java/com/lilcode/example/serviceexample/MyJobIntentService.kt
@@ -1,12 +1,29 @@
 package com.lilcode.example.serviceexample
 
 import android.content.Intent
+import android.util.Log
 import androidx.core.app.JobIntentService
+import java.lang.Exception
 
 // Deprecated 되었음.. WorkManager 에 대해서 알아볼것.
 class MyJobIntentService : JobIntentService(){
-    override fun onHandleWork(intent: Intent) {
 
+    private val TAG = "ServiceExample"
+
+    override fun onHandleWork(intent: Intent) {
+        Log.i(TAG, "Job Service started")
+
+        var i: Int = 0
+
+        while (i <= 3) {
+            try {
+                Thread.sleep(10_000)
+                i++
+            } catch (e: Exception) {
+            }
+
+            Log.i(TAG, "Service running")
+        }
     }
 
 }
