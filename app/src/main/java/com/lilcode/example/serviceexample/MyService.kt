d1704045739e0e081bfc6271893fdb118119f07b
NetLSS <futureprogrammer@naver.com>
2021-08-26 23:25:30 +0900

    Feat: 코루틴을 사용한 서비스를 외부 스레드에서 실행!

--- app/src/main/java/com/lilcode/example/serviceexample/MyService.kt
+++ app/src/main/java/com/lilcode/example/serviceexample/MyService.kt
@@ -4,29 +4,43 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
+import kotlinx.coroutines.*
 import java.lang.Exception
+import kotlin.coroutines.CoroutineContext
 
-class MyService : Service() {
+class MyService : Service(), CoroutineScope {
+
+    private lateinit var job: Job
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Default + job
 
     private val TAG = "ServiceExample"
 
     override fun onCreate() {
         Log.i(TAG, "Service onCreate")
+        job = Job()
     }
 
-    // 제대로 동작하는 서비스를 위해 오버라이딩
+    // 제대로 동작하는 서비를 위해 오버라이딩
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        launch(coroutineContext){
+            performTask(startId)
+        }
+        return Service.START_STICKY
+    }
+
+    suspend fun performTask(startId: Int) {
         Log.i(TAG, "Service onStartCommand " + startId)
         var i: Int = 0
         while (i <= 3) {
             try {
-                Thread.sleep(10_000)
+                delay(10_000)
                 i++
             } catch (e: Exception) {
             }
-            Log.i(TAG,"Service running")
+            Log.i(TAG, "Service running " + startId)
         }
-        return Service.START_STICKY
     }
 
     override fun onBind(intent: Intent): IBinder {
